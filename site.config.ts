9d01bbc1af1cf1ef52ce56d84aa3dd712bfb6732
bi ho <hobi2908@gmail.com>
2024-11-13 21:27:20 +0700

    update url default avatar

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: '/default-avatar.webp',
+  defaultPageIcon: 'https://paste.io.vn/default-avatar.webp',
   defaultPageCover: '/page-cover.jpg',
   defaultPageCoverPosition: 0.5,
 
